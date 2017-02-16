1588506411251ff5f207f1f5bd9190746291a144
Sergey Fedotov <sergey89@gmail.com>
2017-02-16 19:05:34 +0300

    Improved caching strategy without proxy

--- nginx_image_processing/resizer/conf/resize.lua
+++ nginx_image_processing/resizer/conf/resize.lua
@@ -1,40 +1,46 @@
-local LOCK_TIMEOUT = 5      -- 5 s
-local POLLING_DELAY = 0.01  -- 10 ms
+local LOCK_TIMEOUT = 0.5    -- 0.5 s
+local POLLING_DELAY = 0.05  -- 50 ms
 
 ngx.req.discard_body()
 
-local return_uri = ngx.var.return_uri
+local cache = ngx.shared.cache;
+local cache_key = ngx.md5_bin(ngx.var.uri)
 
-local locks = ngx.shared.locks;
-local lock_key = ngx.md5_bin(return_uri)
-local locked = locks:add(lock_key, true, LOCK_TIMEOUT)
+while true do
+    local cached = cache:get(cache_key)
+    if cached then
+        ngx.print(cached)
+        return ngx.exit(ngx.HTTP_OK)
+    end
 
-if not locked then
+    local locked = cache:add(cache_key, false, LOCK_TIMEOUT)
+    if locked then
+        break
+    end
+
     ngx.sleep(POLLING_DELAY)
-    return ngx.exec(return_uri)
 end
 
 local full_path, format = ngx.var.full_path, ngx.var.format
 local width, height = tonumber(ngx.var.width), tonumber(ngx.var.height)
 
-local img, error = magick.load_image(full_path)
+local img, err = magick.load_image(full_path)
+assert(img, err)
 
-if not img then
-    ngx.log(ngx.ERR, error)
-else
-    local w, h = img:get_width(), img:get_height()
-    local scale = math.min(width / w, height / h)
+local w, h = img:get_width(), img:get_height()
+assert(w and w > 0)
+assert(h and h > 0)
 
-    if img:auto_orient()
-        and img:resize(w * scale, h * scale)
-        and img:set_interlace_scheme('PlaneInterlace')
-        and img:set_quality(85)
-        and img:set_format(format)
-        and img:strip()
-    then
-        ngx.print(img:get_blob())
-        return ngx.exit(ngx.HTTP_OK)
-    end
-end
+local scale = math.min(width / w, height / h)
 
-return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+assert(img:auto_orient())
+assert(img:resize(w * scale, h * scale))
+assert(img:set_interlace_scheme('PlaneInterlace'))
+assert(img:set_quality(85))
+assert(img:set_format(format))
+assert(img:strip())
+
+cache:set(cache_key, img:get_blob())
+
+ngx.print(img:get_blob())
+return ngx.exit(ngx.HTTP_OK)
